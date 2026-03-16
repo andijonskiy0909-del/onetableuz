e9374c0879a66a00eb7b85b12132bcbb63a27464
andijonskiy0909-del <andijonskiy0909@gmail.com>
2026-03-16 06:23:53 +0500

    Create restaurants.sql

---
+++ db/seeds/restaurants.sql
@@ -0,0 +1,10 @@
+INSERT INTO restaurants (name, description, address, phone, image_url, cuisine, price_category, rating, is_premium, status, lat, lng)
+VALUES
+  ('Central Bistro', 'Shahar markazidagi qulay bistro', 'Mustaqillik ko''chasi 12, Toshkent', '+998901234567', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836', ARRAY['European','Cafe'], '$$', 4.5, true, 'approved', 41.311081, 69.240562),
+  ('Samarqand Taomi', 'An''anaviy o''zbek taomlari', 'Registan maydoni, Samarqand', '+998901234568', 'https://images.unsplash.com/photo-1541542684-2f7bff3f9a2b', ARRAY['Uzbek','Central Asian'], '$$', 4.7, true, 'approved', 39.6542, 66.9597),
+  ('Spice Road', 'Pan-Osiyo taomlari va kokteyllar', 'Shahar markazi, Toshkent', '+998901234569', 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4', ARRAY['Asian','Pan-Asian'], '$$$', 4.3, false, 'approved', 41.2995, 69.2401),
+  ('Choy va Taomlar', 'Choy va shirinliklar', 'Bozor ko''chasi, Toshkent', '+998901234570', 'https://images.unsplash.com/photo-1504754524776-8f4f37790ca0', ARRAY['Cafe','Dessert'], '$', 4.6, false, 'approved', 41.3115, 69.2798),
+  ('Nomad Grill', 'O''tin olovida pishirilgan go''sht', 'Daryo bo''yi, Buxoro', '+998901234571', 'https://images.unsplash.com/photo-1541542684-2f7bff3f9a2b', ARRAY['Grill','Uzbek'], '$$', 4.4, false, 'approved', 39.7746, 64.4286),
+  ('Little Italy', 'Klassik italyan pasta va pizza', 'Italiya ko''chasi, Toshkent', '+998901234572', 'https://images.unsplash.com/photo-1528605248644-14dd04022da1', ARRAY['Italian'], '$$', 4.2, false, 'approved', 41.3120, 69.2450),
+  ('Rooftop 88', 'Tom ustida kechki ovqat', 'Baland ko''cha, Toshkent', '+998901234573', 'https://images.unsplash.com/photo-1504674900247-0877df9cc836', ARRAY['Bar','International'], '$$$', 4.8, true, 'approved', 41.3150, 69.2400),
+  ('Sushi House', 'Yangi sushi va sashimi', 'Bozor ko''chasi, Samarqand', '+998901234574', 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4', ARRAY['Japanese'
