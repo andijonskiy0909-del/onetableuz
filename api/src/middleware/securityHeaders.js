914019fe29a9ac503932331a292b6ac70d76d9b5
andijonskiy0909-del <andijonskiy0909@gmail.com>
2026-03-25 23:31:36 +0500

    Create securityHeaders.js

---
+++ api/src/middleware/securityHeaders.js
@@ -0,0 +1,13 @@
+const helmet = require('helmet');
+
+module.exports = helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "https:"],
+    },
+  },
+  crossOriginEmbedderPolicy: false,
+});
