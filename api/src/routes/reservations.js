af1ca5dc265881f6b03ecaeb62bb021aba166421
andijonskiy0909-del <andijonskiy0909@gmail.com>
2026-04-15 10:49:03 +0500

    Update reservations.js

--- api/src/routes/reservations.js
+++ api/src/routes/reservations.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const c = require('../controllers/reservationController')
 const { authUser } = require('../middleware/auth')
 
+// Public (for bot cron)
 router.get('/past-unreviewed', c.pastUnreviewed)
 router.put('/:id/review-asked', c.markReviewAsked)
 
+// Protected
 router.post('/', authUser, c.create)
 router.get('/my', authUser, c.myList)
 router.delete('/:id', authUser, c.cancel)
