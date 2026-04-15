3e1549d22e45b34b270bd421e2922b6597b96c58
andijonskiy0909-del <andijonskiy0909@gmail.com>
2026-04-15 10:49:39 +0500

    Add route to list reviews by restaurant ID

--- api/src/routes/reviews.js
+++ api/src/routes/reviews.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const c = require('../controllers/reviewController')
 
 router.post('/', c.create)
+router.get('/restaurant/:id', c.listByRestaurant)
 
 module.exports = router
